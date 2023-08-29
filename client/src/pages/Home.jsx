fbd52c5e9fa1b76fa1a8e87a3d9fbe6ae5e9c0bb
Nicolas Ippoliti <contacto.nicolasippoliti@gmail.com>
2023-08-28 23:12:16 -0300

    minifix

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,3 @@
-// src/components/Home.js
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,4 +1,3 @@
-// src/pages/Profile.jsx
 import { useState, useEffect } from 'react';
 
 const Profile = () => {
